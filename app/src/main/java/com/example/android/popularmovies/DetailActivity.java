40ec4669e0f6065ab854bce47b7f145840aa4fe9
lsitec205.ferreira <denis.ferreira@lsitec.org.br>
2017-12-01 14:34:05 -0200

    Fifth review

--- app/src/main/java/com/example/android/popularmovies/DetailActivity.java
+++ app/src/main/java/com/example/android/popularmovies/DetailActivity.java
@@ -2,6 +2,8 @@ package com.example.android.popularmovies;
 
 import android.content.ContentValues;
 import android.net.Uri;
+import android.os.Parcelable;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,6 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,6 +29,9 @@ import java.text.SimpleDateFormat;
 
 public class DetailActivity extends AppCompatActivity implements FetchTrailerTask.OnTaskCompleted, FetchReviewTask.OnTaskCompleted{
 
+    private static final String SAVED_TRAILER_LAYOUT_MANAGER_STATE = "trailer_layout_state";
+    private static final String SAVED_REVIEW_LAYOUT_MANAGER_STATE = "review_layout_state";
+    private static final String DETAIL_SCROLL_POSITION = "detail_scroll_position";
     private Movie mMovie;
     private TextView mTitleTextView;
     private TextView mYearTextView;
@@ -33,10 +39,16 @@ public class DetailActivity extends AppCompatActivity implements FetchTrailerTas
     private TextView mOverwiewTextView;
     private ImageView mPosterImageView;
     private Button mFavoriteButton;
-    private TrailerAdapter mAdapter;
+    private TrailerAdapter mTrailerAdapter;
     private ReviewAdapter mReviewAdapter;
-    private RecyclerView mRecyclerView;
+    private LinearLayoutManager mTrailerLayoutManager;
+    private LinearLayoutManager mReviewLayoutManager;
+    private RecyclerView mTrailerRecyclerView;
     private RecyclerView mReviewRecyclerView;
+    private Parcelable mTrailerLayoutManagerState;
+    private Parcelable mReviewLayoutManagerState;
+    private ScrollView mDetailScrollView;
+
     private ContentValues values;
 
     private static final String MOVIE_EXTRA = "movie";
@@ -52,21 +64,36 @@ public class DetailActivity extends AppCompatActivity implements FetchTrailerTas
         mRatingTextView = (TextView) findViewById(R.id.movie_rating);
         mPosterImageView = (ImageView) findViewById(R.id.iv_banner_main);
         mFavoriteButton = (Button) findViewById(R.id.favorite_button);
+        mDetailScrollView = (ScrollView) findViewById(R.id.detail_scroll);
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.rc_trailer_grid);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(DetailActivity.this);
-        mRecyclerView.setLayoutManager(layoutManager);
-        mRecyclerView.setHasFixedSize(true);
-        mAdapter = new TrailerAdapter();
-        mRecyclerView.setAdapter(mAdapter);
+        mTrailerRecyclerView = (RecyclerView) findViewById(R.id.rc_trailer_grid);
+        mTrailerLayoutManager = new LinearLayoutManager(DetailActivity.this);
+        mTrailerRecyclerView.setLayoutManager(mTrailerLayoutManager);
+        mTrailerRecyclerView.setHasFixedSize(true);
+        mTrailerAdapter = new TrailerAdapter();
+        mTrailerRecyclerView.setAdapter(mTrailerAdapter);
 
         mReviewRecyclerView = (RecyclerView) findViewById(R.id.rc_review_grid);
-        LinearLayoutManager anotherlayoutManager = new LinearLayoutManager(DetailActivity.this);
-        mReviewRecyclerView.setLayoutManager(anotherlayoutManager);
+        mReviewLayoutManager = new LinearLayoutManager(DetailActivity.this);
+        mReviewRecyclerView.setLayoutManager(mReviewLayoutManager);
         mReviewRecyclerView.setHasFixedSize(true);
         mReviewAdapter = new ReviewAdapter();
         mReviewRecyclerView.setAdapter(mReviewAdapter);
-
+/*
+        if(savedInstanceState != null){
+            mTrailerLayoutManagerState = savedInstanceState.getParcelable(SAVED_TRAILER_LAYOUT_MANAGER_STATE);
+            mReviewLayoutManagerState = savedInstanceState.getParcelable(SAVED_REVIEW_LAYOUT_MANAGER_STATE);
+            final int[] position = savedInstanceState.getIntArray(DETAIL_SCROLL_POSITION);
+            if(position != null) {
+                mDetailScrollView.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mDetailScrollView.scrollTo(position[0], position[1]);
+                    }
+                });
+            }
+        }
+*/
         if(getIntent().hasExtra(MOVIE_EXTRA)) {
             mMovie =  getIntent().getParcelableExtra(MOVIE_EXTRA);
             setTitle(getString(R.string.details)) ;
@@ -121,11 +148,30 @@ public class DetailActivity extends AppCompatActivity implements FetchTrailerTas
     @Override
     public void onTaskCompleted(Review[] result) {
         mReviewAdapter.setReviewData(result);
+        mReviewAdapter.notifyDataSetChanged();
+        if (mReviewLayoutManagerState != null) {
+            mReviewRecyclerView.getLayoutManager().onRestoreInstanceState(mReviewLayoutManagerState);
+        }
     }
 
     @Override
     public void onTaskCompleted(Trailer[] result) {
-        mAdapter.setTrailerData(result);
+        mTrailerAdapter.setTrailerData(result);
+        mTrailerAdapter.notifyDataSetChanged();
+        if (mTrailerLayoutManagerState != null) {
+            mTrailerRecyclerView.getLayoutManager().onRestoreInstanceState(mTrailerLayoutManagerState);
+        }
     }
-
+/*
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        mTrailerLayoutManagerState = mTrailerRecyclerView.getLayoutManager().onSaveInstanceState();
+        mReviewLayoutManagerState = mReviewRecyclerView.getLayoutManager().onSaveInstanceState();
+        outState.putParcelable(SAVED_TRAILER_LAYOUT_MANAGER_STATE, mTrailerLayoutManagerState);
+        outState.putParcelable(SAVED_REVIEW_LAYOUT_MANAGER_STATE, mReviewLayoutManagerState);
+        outState.putIntArray(DETAIL_SCROLL_POSITION,
+                new int[]{mDetailScrollView.getScrollX(), mDetailScrollView.getScrollY()});
+        super.onSaveInstanceState(outState, outPersistentState);
+    }
+*/
 }
